a70a9b94aaecb0a3b0257d94b442e77c7072ce01
Ceb <imcebrian212@outlook.com>
2020-08-14 20:25:33 -0400

    Activity Indicator

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,36 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
-import { CAMPSITES } from '../shared/campsites';
 
 class Contact extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES
-        };
-    }
-
     static navigationOptions = {
         title: 'Contact Us'
-    };
-
+    }
 
     render() {
         return (
             <ScrollView>
-                <Card>
-                    <Text
-                        wrapperStyle={{ margin: 10 }}>
-                        Contact Information</Text>
+                 <Card
+                    title="Contact Information"
+                    wrapperStyle={{margin: 20}}>
                     <Text>1 Nucamp Way</Text>
                     <Text>Seattle, WA 98001</Text>
-                    <Text>U.S.A</Text>
-                    <Text style={{margin: 10}}></Text>
+                    <Text style={{marginBottom: 10}}>U.S.A.</Text>
                     <Text>Phone: 1-206-555-1234</Text>
                     <Text>Email: campsites@nucamp.co</Text>
-                </Card> 
+                </Card>
             </ScrollView>
         );
     }
